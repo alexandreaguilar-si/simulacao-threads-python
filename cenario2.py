742403e67e4030a5830eeeec684cc3aa51848bf2
Alexandre Felisberto de Aguilar <allexandreaguilar@gmail.com>
2026-04-27 17:30:00 -0300

    cenario2.py

---
+++ cenario2.py
@@ -0,0 +1,21 @@
+
+import threading
+import time
+
+def tarefa(thread_id, tempo_execucao):
+ print("Thread", thread_id, "iniciada")
+ time.sleep(tempo_execucao)
+ print("Thread", thread_id,"finalizada")
+
+tempos = [2, 3, 1, 4, 2]
+threads = []
+
+for i, tempo in enumerate(tempos):
+ t = threading.Thread(target=tarefa, args=(i+1, tempo))
+ threads.append(t)
+ t.start()
+
+for t in threads:
+ t.join()
+
+print("Todas as threads finalizaram")
